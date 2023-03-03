2ec6b20321591b35d4ee0d4541e09301c56a056c
MominjonovOyatillo <126873597+MominjonovOyatillo@users.noreply.github.com>
2023-03-03 18:45:32 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -9,90 +9,99 @@ def post_order(request):
     name = request.POST.get('name')
     number = request.POST.get('number')
     Order.objects.create(name=name, number=number)
-    return Response('Success')
+    order = Order.objects.last()
+    data = OrderSerializer(order).data
+    return Response(data)
 
 
 @api_view(['GET'])
 def get_Info(request):
     info = Info.objects.last()
-    ser = InfoSerializer(info)
-    return Response(ser.data)
+    ser = InfoSerializer(info).data
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_order(request):
-    order = Order.objects.all()
-    ser = OrderSerializer(order, many=True)
-    return Response(ser.data)
+    order = Order.objects.last()
+    ser = OrderSerializer(order).data
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_product(request):
-    product = Product.objects.all()
-    ser = ProductSerializer(product, many=True)
-    return Response(ser.data)
+    product = Product.objects.last()
+    ser = ProductSerializer(product).data
+    return Response(ser)
 
 
 
 @api_view(['GET'])
 def get_productinfo(request):
     product_info = Productinfo.objects.all()
-    ser = ProductinfoSerializer(product_info, many=True)
-    return Response(ser.data)
+    ser = ProductinfoSerializer(product_info, many=True).data
+    return Response(ser)
 
 
 
 @api_view(['GET'])
 def get_fakt(request):
     facts = Fakt.objects.all()
-    ser = FaktSerializer(facts, many=True)
-    return Response(ser.data)
+    ser = FaktSerializer(facts, many=True).data
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_aboutus(request):
-    about = Aboutus.objects.all()
-    ser = AboutusSerializer(about, many=True)
-    return Response(ser.data)
+    about = Aboutus.objects.last()
+    ser = AboutusSerializer(about).data
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_productfaq(request):
-    productfaq = Productfaq.objects.all()
-    ser = ProductfaqSerializer(productfaq, many=True)
-    return Response(ser.data)
+    productfaq = Productfaq.objects.last()
+    ser = ProductfaqSerializer(productfaq).data
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_header(request):
-    header = Header.objects.all()
-    ser = HeaderSerializer(header, many=True)
-    return Response(ser.data)
+    header = Header.objects.last()
+    ser = HeaderSerializer(header).data
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_faq(request):
     faq = Faq.objects.all()
     ser = FaqSerializer(faq, many=True)
-    return Response(ser.data)
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_advice(request):
     advice = Advice.objects.all()
     ser = AdviceSerializer(advice, many=True)
-    return Response(ser.data)
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_abouttea(request):
     aboutt = Abouttea.objects.all()
     ser = AboutteaSerializer(aboutt, many=True)
-    return Response(ser.data)
+    return Response(ser)
 
 
 @api_view(['GET'])
 def get_ijtimoiy(request):
     ijtimoiy = Ijtimoiy.objects.all()
     ser = IjtimoiySerializer(ijtimoiy, many=True)
-    return Response(ser.data)
+    return Response(ser)
+
+    
+@api_view(["GET"])
+def get_discount(request):
+    discount = Discount.objects.last()
+    ser = DiscountSerializer(discount).data
+    return Response(ser)
