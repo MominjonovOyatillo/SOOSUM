57c3a6a6ef455c20d4a26d505d7a6f7fd945b2eb
MominjonovOyatillo <126873597+MominjonovOyatillo@users.noreply.github.com>
2023-03-03 18:47:42 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('get-Info/', get_Info),
     path('post-order/', post_order),
     path('get-product/', get_product),
-    path('get-order/', get_order),
     path('get-productinfo/', get_productinfo),
     path('get-fakt/', get_fakt),
     path('get-aboutus/', get_aboutus),
@@ -15,4 +14,5 @@ urlpatterns = [
     path('get-advice/', get_advice),
     path('get-abouttea/', get_abouttea),
     path('get-ijtimoiy/', get_ijtimoiy),
+    path('get-discount/', get_discount)
 ]
